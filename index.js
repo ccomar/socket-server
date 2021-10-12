8a5b1f7095d3165082f56b5edb54355f60b61589
Omar Houmz <omar.houmz@clap.io>
2021-10-12 17:30:37 +0100

    var

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require('express')
 const http = require('http')
-const cors = require('cors')
 const { Server } = require('socket.io')
 
 const app = express()
 const server = http.createServer(app)
-const io = new Server(server, {
-  path: '/test-socket/',
-  serveClient: false,
-  cors: { origin: ['http://localhost:3000', 'http://localhost:3001'] },
-})
+const options = [
+  server,
+  {
+    path: '/test-socket/',
+    serveClient: false,
+    cors: { origin: '*' },
+  },
+]
+const io = new Server(...options)
 
 io.on('connection', (socket) => {
   console.log('a user connected')
